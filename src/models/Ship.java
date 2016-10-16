27f54631343593301eb58360afbf7d94fb768f2d
Kargin Nikita <hei@spark-mail.ru>
2016-10-16 17:55:50 +0300

    Created model Ship

--- src/models/Ship.java
+++ src/models/Ship.java
@@ -1,7 +1,26 @@
 package models;
 
-public class Ship {
+class Ship {
 
+    private final Point[] ship;
+    private final int size;
+    private final ShipValue value;
 
+    public Ship(int size, ShipValue value) {
+        this.size = size;
+        this.value = value;
+        this.ship = new Point[size];
+    }
 
+    Point[] getShip() {
+        return this.ship;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public ShipValue getValue() {
+        return value;
+    }
 }
