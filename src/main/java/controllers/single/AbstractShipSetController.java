d42a7f6008a31d625dce4a21a2b47dc7a0c9e7c2
Kargin Nikita <hei@spark-mail.ru>
2016-10-16 18:57:14 +0300

    Added setShip method with catch

--- src/main/java/controllers/single/AbstractShipSetController.java
+++ src/main/java/controllers/single/AbstractShipSetController.java
@@ -1,4 +1,26 @@
 package controllers.single;
 
-public class AbstractShipSetController {
+import exceptions.AlreadyOccupiedPointException;
+import models.Field;
+import models.Ship;
+
+public abstract class AbstractShipSetController {
+
+    final private Field field;
+
+    public AbstractShipSetController(Field field) {
+        this.field = field;
+    }
+
+    public void setShip(Ship ship) {
+        while (true) {
+            try {
+                field.setShip(ship);
+                return;
+            } catch (AlreadyOccupiedPointException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/main/java/models/Ship.java
+++ src/main/java/models/Ship.java
@@ -1,6 +1,6 @@
 package models;
 
-class Ship {
+public class Ship {
 
     private final Point[] ship;
     private final int size;
