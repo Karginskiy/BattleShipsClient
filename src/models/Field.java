2e85ef26e626ccbf5c84ab95f506fe3bb1da9e0e
Kargin Nikita <hei@spark-mail.ru>
2016-10-16 17:54:33 +0300

    Created class Field

--- src/models/Field.java
+++ src/models/Field.java
@@ -1,6 +1,61 @@
 package models;
 
+import exceptions.AlreadyOccupiedPointException;
+
 public class Field {
+
+    final private ShipValue[][] field;
+    final private int size;
+
+    public Field(int size) {
+        this.size = size;
+        this.field = new ShipValue[size][size];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setShip(Ship ship) throws AlreadyOccupiedPointException {
+
+        for (Point point: ship.getShip()) {
+            checkForEmpty(point);
+        }
+
+        for (Point point: ship.getShip()) {
+            field[point.getX()][point.getY()] = ship.getValue();
+        }
+
+    }
+
+    private ShipValue getPoint(Point point) {
+        return field[point.getX()][point.getY()];
+    }
+
+    private boolean checkForEmpty(Point point) throws AlreadyOccupiedPointException {
+
+        if (getPoint(point) != null) {
+            throw new AlreadyOccupiedPointException("The point has already occupied by the another ship");
+        } else {
+            return true;
+        }
+
+    }
+
+    private boolean checkForNearEmpty(Point point) throws AlreadyOccupiedPointException {
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                try {
+                    if (getPoint(point) == null) {
+                        throw new AlreadyOccupiedPointException("Near is the ship!");
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+        }
+        return true;
+    }
 
 
 
