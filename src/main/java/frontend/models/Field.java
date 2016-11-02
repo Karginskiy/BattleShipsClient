84382b05eb026768e08bf6fc3f3bf02214a3f157
Kargin Nikita <hei@spark-mail.ru>
2016-11-02 14:38:03 +0300

    Changed Field

--- src/main/java/frontend/models/Field.java
+++ src/main/java/frontend/models/Field.java
@@ -2,53 +2,60 @@ package frontend.models;
 
 import frontend.exceptions.AlreadyOccupiedPointException;
 
-public class Field {
+import java.util.Iterator;
 
-    private final ShipValue[][] field;
+public class Field implements Iterable<Point> {
+
+    private final Point[][] field;
     public static int SIZE;
 
     public Field(int size) {
         Field.SIZE = size;
-        this.field = new ShipValue[size][size];
+        this.field = new Point[size][size];
+        initField();
     }
 
     public void setShip(Ship ship) throws AlreadyOccupiedPointException {
 
         for (Point point: ship.getShip()) {
-            checkForEmpty(point);
-            checkForNearEmpty(point);
+            if (!checkForEmpty(point.getX(), point.getY())) return;
+            if (!checkForNearEmpty(point.getX(), point.getY())) return;
         }
 
         for (Point point: ship.getShip()) {
-            field[point.getX()][point.getY()] = ship.getValue();
+            setPoint(point, ship.getValue());
         }
 
     }
 
-    public ShipValue getPoint(Point point) {
-        return field[point.getX()][point.getY()];
+    public Point getPoint(int x, int y) {
+        return field[x][y];
     }
 
     public void setPoint(Point point, ShipValue value) {
-        field[point.getX()][point.getY()] = value;
+        getPoint(point.getX(), point.getY()).setValue(value);
     }
 
-    private boolean checkForEmpty(Point point) throws AlreadyOccupiedPointException {
+    public Iterator<Point> iterator() {
+        return new FieldIterator();
+    }
 
-        if (getPoint(point) != null) {
-            throw new AlreadyOccupiedPointException("The point has already occupied by the another ship");
+    private boolean checkForEmpty(int x, int y) throws AlreadyOccupiedPointException {
+
+        if (getPoint(x, y).getValue() != null) {
+            return false;
         } else {
             return true;
         }
 
     }
 
-    private boolean checkForNearEmpty(Point point) throws AlreadyOccupiedPointException {
+    private boolean checkForNearEmpty(int x, int y) {
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
                 try {
-                    if (getPoint(point) == null) {
-                        throw new AlreadyOccupiedPointException("Near is the ship!");
+                    if (getPoint(x + i, y + j).getValue() != null) {
+                        return false;
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     continue;
@@ -56,5 +63,34 @@ public class Field {
             }
         }
         return true;
+    }
+
+    private class FieldIterator implements Iterator<Point> {
+
+        private int current_i = 0;
+        private int current_j = 0;
+
+        public boolean hasNext() {
+            return current_j < SIZE && current_i < SIZE;
+        }
+
+        public Point next() {
+            if (current_j == SIZE - 1) {
+                return field[current_i++][current_j -= 2];
+            }
+            return field[current_i][current_j++];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private void initField() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                field[i][j] = new Point(i, j);
+            }
+        }
     }
 }
