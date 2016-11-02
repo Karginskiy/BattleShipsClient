3db619a8108a2c5531809d19466d430d37bce5e5
Kargin Nikita <hei@spark-mail.ru>
2016-11-02 14:37:30 +0300

    Created field

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -1,7 +1,27 @@
 package frontend;
 
-public class Main {
+import frontend.controllers.single.AIMoveController;
+import frontend.models.Field;
+import frontend.views.AIPlayerFieldView;
 
+import javax.swing.*;
 
+public class Main extends JFrame {
+
+    private Main() {
+        Field aiField = new Field(8);
+        AIMoveController aiMoveController = new AIMoveController(aiField);
+        setBounds(200, 200, 406, 426);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        add(new AIPlayerFieldView(aiField));
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new Main();
+
+    }
 
 }
